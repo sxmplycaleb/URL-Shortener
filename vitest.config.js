f558d7c3ac86dc847f4363562547124722033ca9
sxmplycaleb <calibregizmo15@gmail.com>
2026-07-24 23:42:06 +0300

    chore: resolve release blockers for v1.1.0

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,9 @@
+import { fileURLToPath } from "node:url";
+
 export default {
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname,
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   test: {
